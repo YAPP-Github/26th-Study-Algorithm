1cb8efac7e2d35cd3b7b1194fdd895afef067663
ppinppini <bnm0218@naver.com>
2025-11-07 01:23:30 +0900

    Week6: H-index

--- week6/황유빈/H-Index.js
+++ week6/황유빈/H-Index.js
@@ -1,3 +1,4 @@
+// n풀이
 function solution(citations) {
     var answer = 0;
     for(let i=0; i<=citations.length;i++){
@@ -7,3 +8,16 @@ function solution(citations) {
     }
     return answer;
 }
+// 다른 풀이 nlogn
+function solution(citations) {
+    citations.sort((a,b)=>b-a)
+    var answer = 0;
+    
+    for(let i=0; i<citations.length;i++){
+        if(citations[i] > i){
+            answer++
+        }
+    }
+    return answer;
+}
+//[6,5,3,1,0]
