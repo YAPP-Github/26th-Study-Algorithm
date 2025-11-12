342c37eaeee361d8005b88114ca593fbbd7c6cec
ppinppini <bnm0218@naver.com>
2025-11-12 15:57:18 +0900

    Week6: 징검다리 건너기(효울성X)

---
+++ week6/황유빈/징검다리 건너기.js
@@ -0,0 +1,32 @@
+function canCross(mid,stones,k){
+    let count=0;
+    for(let i=0;i<stones.length;i++){
+        if(stones[i] < mid){
+            count++
+        }else{
+            count=0
+        }
+        if(count >=k){
+            return false
+        }
+    }
+    return true
+}
+
+function solution(stones,k){
+    
+    let left = 1
+    let right =200000
+    let answer
+    
+    while(left <= right){
+        let mid = Math.floor((left+right)/2)
+        if(canCross(mid,stones,k)){
+            left=mid+1
+            answer = mid
+        }else{
+            right=mid-1
+        }
+    }
+    return answer
+}
